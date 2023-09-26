35f56a7344c6f3455963eff2cf5d18eeb4a6be59
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2023-09-26 16:15:38 +0200

    Add character class

---
+++ Source/Blaster/BlasterGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlasterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ABlasterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
