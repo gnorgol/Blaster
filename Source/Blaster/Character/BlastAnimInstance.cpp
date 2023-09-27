f652fdd109c4f8aae488d03dd8771334ebd6e813
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2023-09-27 14:13:16 +0200

    Add animation

---
+++ Source/Blaster/Character/BlastAnimInstance.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlastAnimInstance.h"
+#include "BlasterCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UBlastAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+}
+
+void UBlastAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (BlasterCharacter == nullptr)
+	{
+		BlasterCharacter = Cast<ABlasterCharacter>(TryGetPawnOwner());
+	}
+	if (BlasterCharacter == nullptr)
+	{
+		return;
+	}
+	FVector Velocity = BlasterCharacter->GetVelocity();
+	Velocity.Z = 0.0f;
+	Speed = Velocity.Size();
+
+	bIsInAir = BlasterCharacter->GetCharacterMovement()->IsFalling();
+
+	bIsAccelerating = BlasterCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.0f ? true : false;
+
+}
