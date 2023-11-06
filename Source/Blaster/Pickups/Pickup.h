9995ef17a5a2e7b774a1086c2d8afdb759292931
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2023-11-06 14:41:15 +0100

    Add Pickups class

---
+++ Source/Blaster/Pickups/Pickup.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+
+class USphereComponent;
+class USoundCue;
+class UStaticMeshComponent;
+UCLASS()
+class BLASTER_API APickup : public AActor
+{
+	GENERATED_BODY()
+		
+public:	
+	APickup();
+	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+	UPROPERTY(EditAnywhere)
+		USphereComponent* OverlapSphere;
+	UPROPERTY(EditAnywhere)
+		USoundCue* PickupSound;
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* PickupMesh;
+
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	
+
+};
