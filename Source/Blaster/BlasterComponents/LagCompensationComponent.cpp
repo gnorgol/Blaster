ea814f59bd8998f776d426495c985a524eca2a00
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2023-11-12 10:43:09 +0100

    Create Class Lag Compensation

---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,25 @@
+
+
+
+#include "LagCompensationComponent.h"
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
