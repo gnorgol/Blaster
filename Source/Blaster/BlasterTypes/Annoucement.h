99b46dd5ab5bee79914c20cdad7644dabc7b9301
Guillaume Norgol <52493523+gnorgol@users.noreply.github.com>
2024-03-29 17:36:01 +0100

    Enhanced Localization and Generation Support
    This commit introduces significant changes to improve localization and generation support across multiple files.
    
    1. `DefaultEditor.ini`: Added new `EngineTargetsSettings` and expanded `GameTargetsSettings` to include additional search directories, include path wildcards, and support more cultures for localization.
    2. `Game_Compile.ini`: Updated to include more cultures for generation.
    3. `Game_Export.ini`, `Game_ExportDialogueScript.ini`, `Game_Gather.ini`, `Game_Import.ini`, `Game_ImportDialogue.ini`, `Game_ImportDialogueScript.ini`: All these files were updated to include more cultures for generation.
    4. `Game_GenerateReports.ini`: Removed some comments.
    5. `Annoucement.h`: Modified for localization by replacing several string constants with calls to `NSLOCTEXT`.

--- Source/Blaster/BlasterTypes/Annoucement.h
+++ Source/Blaster/BlasterTypes/Annoucement.h
@@ -2,14 +2,7 @@
 #define LOCTEXT_NAMESPACE "Announcement"
 namespace Announcement
 {
-	//const FString NewMatchStartsIn(TEXT("New Match Starts In : "));
 	const FString NewMatchStartsIn = NSLOCTEXT("Announcement", "NewMatchStartsIn", "New Match Starts In : ").ToString();
-	//const FString ThereIsNoWinner(TEXT("There is no winner"));
-	//const FString YouAreTheWinner(TEXT("You are the winner!"));
-	//const FString ThereIsATieBetween(TEXT("There is a tie between: "));
-	//const FString ThereIsATieBetweenTheTeams(TEXT("There is a tie between the teams!"));
-	//const FString RedTeamIsTheWinner(TEXT("Red Team win!"));
-	//const FString BlueTeamIsTheWinner(TEXT("Blue Team win!"));
 	const FString ThereIsNoWinner = NSLOCTEXT("Announcement", "ThereIsNoWinner", "There is no winner").ToString();
 	const FString YouAreTheWinner = NSLOCTEXT("Announcement", "YouAreTheWinner", "You are the winner!").ToString();
 	const FString ThereIsATieBetween = NSLOCTEXT("Announcement", "ThereIsATieBetween", "There is a tie between: ").ToString();
